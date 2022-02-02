4860831637c45aeb19b04ce71a4279e74a72ddf3
ArturDokhno <artur.dohno@icloud.com>
2022-02-02 12:27:15 +0500

    Initial Commit

---
+++ SwiftUI_Course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Course
+//
+//  Created by Артур Дохно on 02.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Course/SwiftUI_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CourseApp.swift
+//  SwiftUI_Course
+//
+//  Created by Артур Дохно on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
